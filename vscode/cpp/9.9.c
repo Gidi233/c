75d12eb0fa3622405969331f952f0183066721ae
Gidi233 <1989871763@qq.com>
2022-11-29 08:56:16 +0800

    practice

---
+++ vscode/cpp/9.9.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+    int c;
+double power(int a,int b){
+    
+if(a==0)
+return 0;
+if(a!=0&&b==0)
+return 1;
+if(b>1){
+return power(a*c,b-1);
+}
+if(b<-1){
+return power(a*c,b+1);
+}
+if(b==1)
+return a;
+if(b==-1)
+return 1.0/a;
+}
+int main(){
+    int a,b;
+    printf("输入一个底数和它的幂");
+    scanf("%d%d",&a,&b);
+    c=a;
+    printf("结果为%10lf",power(a,b));
+}
