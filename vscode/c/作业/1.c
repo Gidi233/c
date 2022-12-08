8e2a729e2b6de727cf2144fa09cdf17fab4cc61e
Gidi233 <1989871763@qq.com>
2022-12-08 21:37:29 +0800

    practice

--- vscode/c/作业/1.c
+++ vscode/c/作业/1.c
@@ -1,17 +1,48 @@
-#include<stdio.h>
-int main(){
-    int max,a[11],b=0,x;
-    scanf("%d",&x);
+#include <stdio.h>
 
-    for(int i=0;i<x;i++){
-        scanf("%d",&a[i]);
+int factorsum( int number );
+void PrintPN( int m, int n );
+    
+int main()
+{
+    int m, n;
+
+    scanf("%d %d", &m, &n);
+    if ( factorsum(m) == m ) printf("%d is a perfect number\n", m);
+    if ( factorsum(n) == n ) printf("%d is a perfect number\n", n);
+    PrintPN(m, n);
+
+    return 0;
+}
+
+int factorsum( int number ){
+int i,a=0;
+for(i=1;i<number;i++){
+    if(number%i==0){
+        a+=i;
     }
-    max=a[0];
-    for(int i=0;i<x;i++){
-        if(a[i]>max){
-            max=a[i];
-            b=i;
-        }
+
+}return a;
+}
+
+
+void PrintPN( int m, int n ){
+for(int i=m;i<=n;i++){
+    int a=0;
+    for(int t=1;t<i;t++){
+    if(i%t==0){
+        a+=t;
     }
-    printf("%d %d",max,b);
     }
+    if(a==i){
+        printf("%d = 1",a);
+        for(int t=2;t<i;t++){
+    if(i%t==0){
+        printf(" + %d",t);
+    }
+
+    }
+    printf("\n");
+}
+}
+}
