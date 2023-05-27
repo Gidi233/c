91ea04975a90b0050d5edd3f65d87bb9df06622a
Gidi233 <1989871763@qq.com>
2023-05-27 14:55:20 +0800

    用的STL的queue，用了模板

--- linux group/SPSCQueue.cpp
+++ linux group/SPSCQueue.cpp
@@ -2,86 +2,79 @@
 #include <string>
 #include <queue>
 #include<pthread.h>
-#include<iostream>
 //#include <mutex>
 //#include <condition_variable>
 using namespace std;
 
-// // Single-producer , single-consumer Queue
-// template<class T>
-// class SPSCQueue {
-// public:
-//     explicit SPSCQueue(int capacity);
-//     virtual bool Push(std::unique_ptr<T>) = 0;
-//     virtual std::unique_ptr<T> pop() = 0;
-//     virtual ~SPSCQueue() = 0;
-// };
 
-int max_;
-pthread_t con,pro;
-    int now=0;
-    queue<unique_ptr<char>> queue_;
-    pthread_mutex_t mutex= PTHREAD_MUTEX_INITIALIZER;
-    pthread_cond_t cond= PTHREAD_COND_INITIALIZER;
-    
+template <typename T>
+class SPSCQueue {
+public:
+    SPSCQueue(int capacity) : max_(capacity) {}
 
-
-
-    bool Push(unique_ptr<char>& value) {
+    bool Push(T value) {
         pthread_mutex_lock(&mutex);
         while (queue_.size() >= max_) {
             pthread_cond_wait(&cond,&mutex);
         }
-        now++;
         queue_.push(move(value));
         pthread_mutex_unlock(&mutex);
         pthread_cond_signal(&cond);
         return true;
     }
 
-    unique_ptr<char> Pop() {
+    T Pop() {
         pthread_mutex_lock(&mutex);
         while (queue_.empty()) {
             pthread_cond_wait(&cond,&mutex);
         }
-        now--;
-        unique_ptr<char> value= move(queue_.front());
-        queue_.pop();//
+        T value = move(queue_.front());
+        queue_.pop();
         pthread_mutex_unlock(&mutex);
         pthread_cond_signal(&cond);
         return value;
     }
 
+    ~SPSCQueue(){}
 
+private:
+    int max_;
+    queue<T> queue_;
+    pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
+    pthread_cond_t cond=PTHREAD_COND_INITIALIZER;
+    //unique_ptr<T> value;
+};
 
 void *producer(void *arg){
     while(1){
     for(char i='a';i<'z';i++){
         unique_ptr<char> pchar(new char(i));
-        Push(pchar);
-    }        
-    cout<<endl;
+        ((SPSCQueue<unique_ptr<char>> *)arg)->Push(move(pchar));
+    }
+    unique_ptr<char> pchar(new char('\n'));
+    ((SPSCQueue<unique_ptr<char>> *)arg)->Push(move(pchar));
     }
 
 }
 
 void *consumer(void *arg){
     while(1){
-        printf("%c ",*(Pop()));
+        printf("%c ",*(((SPSCQueue<unique_ptr<char>> *)arg)->Pop()));
     }
 }
 
 int main(){
-    int num=0;
-    //scanf("num & max:%d%d",&num,&max);
-    //scanf("max:%d",&max_);
-    cin>>max_;
-    pthread_t con,pro;
-        pthread_create(&pro,nullptr,producer,nullptr);
-        pthread_create(&con,nullptr,consumer,nullptr);
-    
-        pthread_join(pro,nullptr);
-        pthread_join(con,nullptr);
-    
+    int num=1,max;
+    scanf("%d",&max);
+    SPSCQueue<unique_ptr<char>> dan(max);
+    pthread_t con[100],pro[100];
+    for(int i=0;i<num;i++){
+        pthread_create(&pro[i],nullptr,producer,&dan);
+        pthread_create(&con[i],nullptr,consumer,&dan);
+    }
+    for(int i=0;i<num;i++){
+        pthread_join(pro[i],nullptr);
+        pthread_join(con[i],nullptr);
+    }
     return 0;
 }
