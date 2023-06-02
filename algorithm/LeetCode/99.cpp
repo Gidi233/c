1c3db5a2dfd21a15339e7e1561ec192f25a1d812
Gidi233 <1989871763@qq.com>
2023-06-02 23:45:10 +0800

    1

---
+++ algorithm/LeetCode/99.cpp
@@ -0,0 +1,143 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& nums) {
+        if (root == nullptr) {
+            return;
+        }
+        inorder(root->left, nums);
+        nums.push_back(root->val);
+        inorder(root->right, nums);
+    }
+
+    pair<int,int> findTwoSwapped(vector<int>& nums) {
+        int n = nums.size();
+        int index1 = -1, index2 = -1;
+        for (int i = 0; i < n - 1; ++i) {
+            if (nums[i + 1] < nums[i]) {
+                index2 = i + 1;
+                if (index1 == -1) {
+                    index1 = i;
+                } else {
+                    break;
+                }
+            }
+        }
+        int x = nums[index1], y = nums[index2];
+        return {x, y};
+    }
+    
+    void recover(TreeNode* r, int count, int x, int y) {
+        if (r != nullptr) {
+            if (r->val == x || r->val == y) {
+                r->val = r->val == x ? y : x;
+                if (--count == 0) {
+                    return;
+                }
+            }
+            recover(r->left, count, x, y);
+            recover(r->right, count, x, y);
+        }
+    }
+
+    void recoverTree(TreeNode* root) {
+        vector<int> nums;
+        inorder(root, nums);
+        pair<int,int> swapped= findTwoSwapped(nums);
+        recover(root, 2, swapped.first, swapped.second);
+    }
+};
+
+//左节点都比父节点小，右节点都比父节点大用中序遍历得到从小到大的排列
+//遍历把元素按顺序存入数组，通过规律找到应交换的两个值，在遍历找到相应的值替换。
+
+class Solution {
+public:
+    void recoverTree(TreeNode* root) {
+        stack<TreeNode*> stk;
+        TreeNode* x = nullptr;
+        TreeNode* y = nullptr;
+        TreeNode* pred = nullptr;
+
+        while (!stk.empty() || root != nullptr) {
+            while (root != nullptr) {
+                stk.push(root);
+                root = root->left;
+            }
+            root = stk.top();
+            stk.pop();
+            if (pred != nullptr && root->val < pred->val) {
+                y = root;
+                if (x == nullptr) {
+                    x = pred;
+                }
+                else break;
+            }
+            pred = root;
+            root = root->right;
+        }
+
+        swap(x->val, y->val);
+    }
+};
+
+//就不用新建一个数组了，记录上一个值，在遍历时遇到不符合条件的直接就记录下来，遍历完直接交换。
+//（用迭代手动维护一个栈,应该可以避免爆栈吧）
+
+class Solution {
+public:
+    void recoverTree(TreeNode* root) {
+        TreeNode *x = nullptr, *y = nullptr, *pred = nullptr, *predecessor = nullptr;
+
+        while (root != nullptr) {
+            if (root->left != nullptr) {
+                // predecessor 节点就是当前 root 节点向左走一步，然后一直向右走至无法走为止
+                predecessor = root->left;
+                while (predecessor->right != nullptr && predecessor->right != root) {
+                    predecessor = predecessor->right;
+                }
+                
+                // 让 predecessor 的右指针指向 root，继续遍历左子树
+                if (predecessor->right == nullptr) {
+                    predecessor->right = root;
+                    root = root->left;
+                }
+                // 说明左子树已经访问完了，我们需要断开链接
+                else {
+                    if (pred != nullptr && root->val < pred->val) {
+                        y = root;
+                        if (x == nullptr) {
+                            x = pred;
+                        }
+                    }
+                    pred = root;
+
+                    predecessor->right = nullptr;
+                    root = root->right;
+                }
+            }
+            // 如果没有左孩子，则直接访问右孩子
+            else {
+                if (pred != nullptr && root->val < pred->val) {
+                    y = root;
+                    if (x == nullptr) {
+                        x = pred;
+                    }
+                }
+                pred = root;
+                root = root->right;
+            }
+        }
+        swap(x->val, y->val);
+    }
+};
