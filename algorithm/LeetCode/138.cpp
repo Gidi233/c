f606fa3d1ea352dd382cb104abd58a138b31576d
Gidi233 <1989871763@qq.com>
2023-09-19 11:38:55 +0800

    链表操作

--- algorithm/LeetCode/138.cpp
+++ algorithm/LeetCode/138.cpp
@@ -51,3 +51,60 @@ public:
         return ans;
     }
 };
+// 记录原链表每一个节点的下标，和新建链表每一个下标对应的节点，先建表，再循环一次从原表的map取下标，用下表得到节点赋给random
+
+class Solution
+{
+public:
+    unordered_map<Node *, Node *> cachedNode;
+
+    Node *copyRandomList(Node *head)
+    {
+        if (head == nullptr)
+        {
+            return nullptr;
+        }
+        if (!cachedNode.count(head))
+        {
+            Node *headNew = new Node(head->val);
+            cachedNode[head] = headNew;
+            headNew->next = copyRandomList(head->next);
+            headNew->random = copyRandomList(head->random);
+        }
+        return cachedNode[head];
+    }
+};
+// 原表新表的下标一一对应，为了防止重复拷贝，我们需要首先检查当前节点是否被拷贝过，如果已经拷贝过，我们可以直接从哈希表中取出拷贝后的节点的指针并返回即可。
+// 如果这两个节点中的任何一个节点的新节点没有被创建，我们都立刻递归地进行创建。（实际上也是先建了表，再给random赋值）
+
+class Solution
+{
+public:
+    Node *copyRandomList(Node *head)
+    {
+        if (head == nullptr)
+        {
+            return nullptr;
+        }
+        for (Node *node = head; node != nullptr; node = node->next->next)
+        {
+            Node *nodeNew = new Node(node->val);
+            nodeNew->next = node->next;
+            node->next = nodeNew;
+        }
+        for (Node *node = head; node != nullptr; node = node->next->next)
+        {
+            Node *nodeNew = node->next;
+            nodeNew->random = (node->random != nullptr) ? node->random->next : nullptr;
+        }
+        Node *headNew = head->next;
+        for (Node *node = head; node != nullptr; node = node->next)
+        {
+            Node *nodeNew = node->next;
+            node->next = node->next->next; // 还原原链表
+            nodeNew->next = (nodeNew->next != nullptr) ? nodeNew->next->next : nullptr;
+        }
+        return headNew;
+    }
+};
+// 通过在原链表的每一个节点后再复制一个，就不用用一个map来存对应的节点（下标），原链表的random->next就是新链表的random，再重新给两个链表的next重新赋值
