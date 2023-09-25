ed9ad4f26831762cba80c20b7394ab33884decca
Gidi233 <1989871763@qq.com>
2023-09-25 19:20:36 +0800

    动态规划，回溯

--- algorithm/LeetCode/22.cpp
+++ algorithm/LeetCode/22.cpp
@@ -17,3 +17,75 @@ public:
         return vec;
     }
 };
+// 参数传递待添加'(',待添加')'，和已生成的string.
+
+class Solution
+{
+    void backtrack(vector<string> &ans, string &cur, int open, int close, int n)
+    {
+        if (cur.size() == n * 2)
+        {
+            ans.push_back(cur);
+            return;
+        }
+        if (open < n)
+        {
+            cur.push_back('(');
+            backtrack(ans, cur, open + 1, close, n);
+            cur.pop_back();
+        }
+        if (close < open)
+        {
+            cur.push_back(')');
+            backtrack(ans, cur, open, close + 1, n);
+            cur.pop_back();
+        }
+    }
+
+public:
+    vector<string> generateParenthesis(int n)
+    {
+        vector<string> result;
+        string current;
+        backtrack(result, current, 0, 0, n);
+        return result;
+    }
+};
+
+// 官方答案，跟上面的想法一样
+
+class Solution
+{
+    shared_ptr<vector<string>> cache[100] = {nullptr};
+
+public:
+    shared_ptr<vector<string>> generate(int n)
+    {
+        if (cache[n] != nullptr)
+            return cache[n];
+        if (n == 0)
+        {
+            cache[0] = shared_ptr<vector<string>>(new vector<string>{""});
+        }
+        else
+        {
+            auto result = shared_ptr<vector<string>>(new vector<string>);
+            for (int i = 0; i != n; ++i)
+            {
+                auto lefts = generate(i);
+                auto rights = generate(n - i - 1);
+                for (const string &left : *lefts)
+                    for (const string &right : *rights)
+                        result->push_back("(" + left + ")" + right);
+            }
+            cache[n] = result;
+        }
+        return cache[n];
+    }
+    vector<string> generateParenthesis(int n)
+    {
+        return *generate(n);
+    }
+};
+
+// 用一个结构记录之前算出的n的vector<string>，分别插入(a)b的a、b中（为0-至n-1的vector<string>）
