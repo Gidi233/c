2672aee9a02eb5f630138ff49557f6eb5c692b23
Gidi233 <1989871763@qq.com>
2023-11-26 21:13:28 +0800

    动态规划，中心扩散

--- algorithm/LeetCode/5.cpp
+++ algorithm/LeetCode/5.cpp
@@ -100,3 +100,71 @@ public:
     }
 };
 // 从中心扩散，我最开始想的是窗口不断缩小+向中间逼近 是个傻逼的n^3
+
+class Solution
+{
+public:
+    int expand(const string &s, int left, int right)
+    {
+        while (left >= 0 && right < s.size() && s[left] == s[right])
+        {
+            --left;
+            ++right;
+        }
+        return (right - left - 2) / 2;
+    }
+
+    string longestPalindrome(string s)
+    {
+        int start = 0, end = -1;
+        string t = "#";
+        for (char c : s)
+        {
+            t += c;
+            t += '#';
+        }
+        t += '#';
+        s = t;
+
+        vector<int> arm_len;
+        int right = -1, j = -1;
+        for (int i = 0; i < s.size(); ++i)
+        {
+            int cur_arm_len;
+            if (right >= i)
+            {
+                int i_sym = j * 2 - i;
+                int min_arm_len = min(arm_len[i_sym], right - i);
+                cur_arm_len = expand(s, i - min_arm_len, i + min_arm_len);
+            }
+            else
+            {
+                cur_arm_len = expand(s, i, i);
+            }
+            arm_len.push_back(cur_arm_len);
+            if (i + cur_arm_len > right)
+            {
+                j = i;
+                right = i + cur_arm_len;
+            }
+            if (cur_arm_len * 2 + 1 > end - start)
+            {
+                start = i - cur_arm_len;
+                end = i + cur_arm_len;
+            }
+        }
+
+        string ans;
+        for (int i = start; i <= end; ++i)
+        {
+            if (s[i] != '#')
+            {
+                ans += s[i];
+            }
+        }
+        return ans;
+    }
+};
+// Manacher 算法
+// 当在位置 i 开始进行中心拓展时，我们可以先找到 i 关于 j 的对称点 2 * j - i。那么如果点 2 * j - i 的臂长等于 n，我们就可以知道，点 i 的臂长至少为 min(j + length - i, n)。那么我们就可以直接跳过 i 到 i + min(j + length - i, n) 这部分，从 i + min(j + length - i, n) + 1 开始拓展。
+// 就是利用最长的回文串，在查询右侧时，若在最长的回文串范围内，先获取其相对最长回文串中心对称位置的臂长（回文长度），也算是DP吧
