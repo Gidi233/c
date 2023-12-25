0e1c1570210debe118fc57c317358d1a3166213a
Gidi233 <1989871763@qq.com>
2023-12-25 13:33:25 +0800

    二分

---
+++ algorithm/LeetCode/704.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int l = 0, r = nums.size() - 1;
+        while (l < r)
+        {
+            int mid = (l + r) / 2;//l+(r-l)/2
+            if (nums[mid] < target)
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        return nums[l] == target ? l : -1;
+    }
+};
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        auto ans = lower_bound(nums.begin(), nums.end(), target);
+        if (ans != nums.end() && *ans == target)
+            return ans - nums.begin();
+        else
+            return -1;
+    }
+};
+int main()
+{
+    Solution solution;
+}
