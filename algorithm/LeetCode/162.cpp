b7dc08ae532605ab57a25a704ac923e2c3b96a23
Gidi233 <1989871763@qq.com>
2024-03-18 08:50:50 +0800

    二分找峰值

---
+++ algorithm/LeetCode/162.cpp
@@ -0,0 +1,31 @@
+class Solution {
+ public:
+  int findPeakElement(vector<int>& nums) {
+    int n = nums.size();
+
+    // 辅助函数，输入下标 i，返回一个二元组 (0/1, nums[i])
+    // 方便处理 nums[-1] 以及 nums[n] 的边界情况
+    auto get = [&](int i) -> pair<int, int> {
+      if (i == -1 || i == n) {
+        return {0, 0};
+      }
+      return {1, nums[i]};
+    };
+
+    int left = 0, right = n - 1, ans = -1;
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      if (get(mid - 1) < get(mid) && get(mid) > get(mid + 1)) {
+        ans = mid;
+        break;
+      }
+      if (get(mid) < get(mid + 1)) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return ans;
+  }
+};
+// 二分找峰值 可能会错过，但最终的复杂度是logn
